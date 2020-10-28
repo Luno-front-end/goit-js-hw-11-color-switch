cb0e0e90a40f25cee915b01fc85bfa3bda9d2ffe
Maksym Zaika <Onlyrobochaposhta@gmail.com>
2020-10-29 00:23:35 +0200

    finished_full_task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-function add(a, b, c) {
-  // Дополни эту строку
-  console.log(a + b + c);
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-action = start]'),
+  btnStop: document.querySelector('[data-action = stop]'),
+};
+
+refs.btnStart.addEventListener('click', onStartSlide);
+refs.btnStop.addEventListener('click', onStopSlide);
+
+let intervalId;
+
+function onStartSlide(e) {
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+  }, 1000);
+  refs.btnStart.disabled = true;
 }
 
-add(15, 27, 10);
+function onStopSlide(e) {
+  clearInterval(intervalId);
+  refs.btnStart.disabled = false;
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
